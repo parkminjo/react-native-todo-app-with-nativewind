031f8eed3eee7b5af6530820daee6fa5bcd7e2fe
parkminjo <mj.park@sigcrew.com>
2026-01-07 15:28:30 +0900

    init: 초기 세팅 및 nativewind 적용

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import { Text, View } from "react-native";
+import "./global.css";
+
+const App = () => {
+  return (
+    <View>
+      <Text>App</Text>
+    </View>
+  );
+};
+
+export default App;
